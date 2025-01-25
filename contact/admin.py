8729b426c569567434f7a75d09ab4c9ca09dffab
Monica Carvalho <monikkarvalho2710@gmail.comgit config --global user.email>
2025-01-25 14:21:06 -0300

    criado arq models admin

--- contact/admin.py
+++ contact/admin.py
@@ -5,6 +5,6 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'first_name', 'last_name', 'phone',
 
 # Register your models here.
