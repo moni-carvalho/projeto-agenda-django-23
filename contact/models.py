73a2ea251d56de4930fcb6a876f7c04aee966ea4
Monica Carvalho <monikkarvalho2710@gmail.comgit config --global user.email>
2025-01-28 17:27:02 -0300

    Criado ScriptPython com dados aleatórios

--- contact/models.py
+++ contact/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # id (primary key) - - automático
 # Create your models here.
 # first_name (string), last_name (string), phone (string)
 # email (email), create_date (date), descriptin (text)
 #depois
-# category (foreign key), show (boolean), owner (foreign key),
+# category (foreign key), show (boolean),picture (imagem) 
+# Depois
+# owner (foreign key),
 # picture (imagem)
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
@@ -16,8 +29,24 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank= True)
     create_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='picture/%Y/%m/')
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
-    def __str__(self)-> str:
+    def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
+    
+
+
+    
     
 
--- project/settings.py
+++ project/settings.py
@@ -1,3 +1,5 @@
+
+
 """
 Django settings for project project.
 
@@ -110,7 +112,7 @@ LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
-USE_I18N = True
+USE_I18N = False
 
 USE_TZ = True
 
@@ -122,7 +124,17 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
+STATIC_ROOT = BASE_DIR / 'static' #collectstatic
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from project.local_settings import *
+except ImportError:
+    ...    
